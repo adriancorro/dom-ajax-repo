e958565cbd9e2faad8e7fb44351da289528bfb3c
adriancorro <adriancorrog@gmail.com>
2021-04-15 10:58:55 +0200

    desarrolando Parte 2

--- js/my-github.js
+++ js/my-github.js
@@ -1,7 +1,7 @@
 // Write code here to communicate with Github
 
 const getRepos = function(repoName) {
-  return fetch("https://api.github.com/users/" + repoName + "/repos")
+  return fetch(`https://api.github.com/users/${repoName}/repos`)
     .then(data => data.json())
     .then(function(response) {
       return response.map(function(rep) {
@@ -13,7 +13,6 @@ const getRepos = function(repoName) {
         let a = document.createElement("a");
         a.setAttribute("target", `_blank`);
         a.setAttribute("href", `${rep.html_url}`);
-
         a.innerText = rep.html_url;
         myElement.appendChild(a);
         return rep.name;
@@ -21,9 +20,21 @@ const getRepos = function(repoName) {
     });
 };
 
+const getNumberRepos = function(repoName) {
+  return fetch(`https://api.github.com/users/${repoName}`)
+    .then(data => data.json())
+    .then(function(response) {
+      let reposCount = document.querySelector("#repos-count");
+      console.log("holaaa");
+      reposCount.innerText = response.public_repos;
+      return response;
+    });
+};
+
 let userRep = "adriancorro";
-const adriancodeRepos = getRepos(`${userRep}`);
+getRepos(`${userRep}`);
+getNumberRepos(`${userRep}`);
 let myElement = document.querySelector("#repos-list");
 let nuevo_parrafo = document.createElement("p");
-nuevo_parrafo.innerText = `Here is a list of my repertoire. My user is: ${userRep}`;
+nuevo_parrafo.innerText = `Here is a list of my repos. My user is: ${userRep}`;
 document.querySelector(".my-auto").insertBefore(nuevo_parrafo, myElement);
