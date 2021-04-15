374867ee02a6619de6fa577e00c809f237af3409
adriancorro <adriancorrog@gmail.com>
2021-04-15 10:18:29 +0200

    Desarrollando Requisito 3

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,29 @@
 // Write code here to communicate with Github
+
+const getRepos = function(repoName) {
+  return fetch("https://api.github.com/users/" + repoName + "/repos")
+    .then(data => data.json())
+    .then(function(response) {
+      return response.map(function(rep) {
+        let myElement = document.querySelector("#repos-list");
+        let paragraph = document.createElement("li");
+        paragraph.innerText = rep.name;
+        myElement.appendChild(paragraph);
+
+        let a = document.createElement("a");
+        a.setAttribute("target", `_blank`);
+        a.setAttribute("href", `${rep.html_url}`);
+
+        a.innerText = rep.html_url;
+        myElement.appendChild(a);
+        return rep.name;
+      });
+    });
+};
+
+let userRep = "adriancorro";
+const adriancodeRepos = getRepos(`${userRep}`);
+let myElement = document.querySelector("#repos-list");
+let nuevo_parrafo = document.createElement("p");
+nuevo_parrafo.innerText = `Here is a list of my repertoire. My user is: ${userRep}`;
+document.querySelector(".my-auto").insertBefore(nuevo_parrafo, myElement);
